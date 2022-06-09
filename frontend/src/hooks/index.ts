994014cb1efbd680eae1915abd91a50dd1c958fa
Поляков Андрей Сергеевич <a.polyakov@bftcom.com>
2022-06-09 21:15:54 +0300

    Добавил хук

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,22 @@
+import { ref } from 'vue';
+
+/**
+ * Используется при событиях: mouseover, mouseout
+ */
+ export function useHover() {
+  const is_hover = ref(false);
+
+  function setHovering() {
+    is_hover.value = true;
+  }
+
+  function unsetHovering() {
+    is_hover.value = false;
+  }
+
+  return {
+    is_hover,
+    setHovering,
+    unsetHovering,
+  };
+}
