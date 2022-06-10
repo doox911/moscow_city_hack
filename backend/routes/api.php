2d03fb7082ce6e7172728282bff700fbf538549c
karchevskiy.a <karchevskiy.a@stis.ru>
2022-06-11 01:57:46 +0300

    main

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,8 +8,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/', [AuthController::class, 'getUser']);
     Route::put('/', [AuthController::class, 'updateUser']);
   });
+
+  Route::post('/register', [AuthController::class, 'register']);
+  Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
