9c754190969c17d10818e0727b07ba3fcec02858
karchevskiy.a <karchevskiy.a@stis.ru>
2022-06-12 18:55:33 +0300

    main

--- backend/app/Http/Resources/GoodResource.php
+++ backend/app/Http/Resources/GoodResource.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class GoodResource extends JsonResource {
+
   /**
    * Transform the resource into an array.
    *
@@ -14,17 +15,24 @@ class GoodResource extends JsonResource {
    * @return array
    */
   public function toArray($request): array {
-    /**
-     * @var Good
-     */
+    /** @var Good  */
     $good = $this->resource;
 
-    //todo добавить поля
     return [
       'id' => $good->id,
       'brand' => $good->brand,
       'name' => $good->name,
+      'description' => $good->description,
       'additional_info' => $good->additional_info,
+      'price' => $good->price,
+      'price_description' => $good->price_description,
+      'keywords_for_search' => $good->keywords_for_search,
+      'data_source_item_url' => $good->data_source_item_url,
+      'data_source_item_last_edit' => $good->data_source_item_last_edit,
+      'price_min_party' => $good->price_min_party,
+      'properties' => $good->properties,
+
+      // источник данных (например: адрес сайта, Excel файл)
       'data_source' => $this->whenLoaded('data_source', DataSourceResource::make($good->data_source)),
     ];
   }
