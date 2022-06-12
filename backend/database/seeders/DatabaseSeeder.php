855db513420260bce4c3231a2a185d017a59167d
karchevskiy.a <karchevskiy.a@stis.ru>
2022-06-13 00:22:30 +0300

    main

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -6,21 +6,15 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder {
+
   /**
    * Seed the application's database.
    *
    * @return void
    */
   public function run() {
-    // \App\Models\User::factory(10)->create();
-
-    // \App\Models\User::factory()->create([
-    //     'name' => 'Test User',
-    //     'email' => 'test@example.com',
-    // ]);
-
-    // $this->call(TestDataSeeder::class);
     $this->call(MenuSeeder::class);
+
     Artisan::call('company:fill_from_excel');
   }
 }
