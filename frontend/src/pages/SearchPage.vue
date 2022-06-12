d6b431dcd8f6d0a06ee163e2d07d650da760fc5a
Поляков Андрей <doox911@gmail.com>
2022-06-12 15:21:34 +0300

    fix

--- frontend/src/pages/SearchPage.vue
+++ frontend/src/pages/SearchPage.vue
@@ -1,6 +1,22 @@
-<template>Search</template>
+<template>
+  <div class="q-pa-sm">
+    <h3 class="non-selectable text-grey-9">Поиск компаний, продукции и услуг</h3>
+    <div class="row q-pt-sm">
+      <div class="col">
+        <q-input
+          v-model="search"
+          :loading="loading"
+          placeholder="Введите поисковой запрос"
+          debounce="500"
+        />
+      </div>
+    </div>
+  </div>
+
+</template>
 
 <script setup lang="ts">
+  import { ref } from 'vue';
 
   /**
    * Hooks
@@ -8,5 +24,9 @@
   import { useUserSearchPageGuard } from 'Src/hooks';
 
   useUserSearchPageGuard();
+
+  const search = ref('');
+
+  const loading = ref(false);
 
 </script>
