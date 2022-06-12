d010567eb863f8e3a68f8391f3520715fe461276
Поляков Андрей <doox911@gmail.com>
2022-06-12 22:18:26 +0300

    fix

--- frontend/src/api/parsing.ts
+++ frontend/src/api/parsing.ts
@@ -17,13 +17,18 @@ import type { DefaultApiResponse } from 'Src/types';
 /**
  * Запуск парсинга
  */
-export async function apiRunParsing(str: string, config?: AxiosRequestConfig) {
+export async function apiRunParsing(str: string, timeinterval_id: ReturnType<typeof setInterval> | null, config?: AxiosRequestConfig) {
   await requestWrapper({
     success: async () => {
       await new ApiRequest(config).post(`api/parse/${str}`);
     },
     error_message: 'Ошибка запуска парсинга',
     success_message: 'Парсинг успешно запущен',
+    error: () => {
+      if (timeinterval_id) {
+        clearInterval(timeinterval_id);
+      }
+    },
   });
 }
 
@@ -33,7 +38,7 @@ export async function apiRunParsing(str: string, config?: AxiosRequestConfig) {
  export async function apiStopParsing(config?: AxiosRequestConfig) {
   await requestWrapper({
     success: async () => {
-      await new ApiRequest(config).post('api/cancelparsing');
+      await new ApiRequest(config).get('api/cancel_parsing');
     },
     error_message: 'Ошибка остановки парсинга',
     success_message: 'Парсинг успешно остановлен',
